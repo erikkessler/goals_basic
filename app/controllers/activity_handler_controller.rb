2cf32e92a4dffc9fd053d8ccca42bf5d3d95ca45
Erik Kessler <erik.kessler1@gmail.com>
2014-06-03 20:42:48 -0400

    Task Form Validation

--- app/controllers/activity_handler_controller.rb
+++ app/controllers/activity_handler_controller.rb
@@ -3,15 +3,20 @@ class ActivityHandlerController < ApplicationController
   def new
     @activities = Activity.where("state is ? AND rep_parent_id is ?",
                                  Activity::INCOMPLETE, nil)
+    @errors = { }
+    @values = { }
   end
   
   def create
-    
+    @activities = Activity.where("state is ? AND rep_parent_id is ?",
+                                 Activity::INCOMPLETE, nil)
+    @values = params
     handler = ActivityHandler.find(1)
-    if handler.create_activity(params)
+    @errors = handler.create_activity(params)
+    if @errors.empty?
       redirect_to 'http://google.com'
     else
-      redirect_to '/activity_handler/new'
+      render 'new'
     end
   end
 end
--- app/models/my_modules.rb
+++ app/models/my_modules.rb
@@ -8,56 +8,69 @@ module MyModules
       # switch on type_id
       case type_id
 
-      when ActivityHandler::FULL_TASK or Activity::PARTIAL_TASK
-        # ensure it has a show_date
-        if params[:show_date].nil?
-          Rails.logger.debug "No show date for full task"
-          return false
-        end
-
-        # ensure it has a name
-        if params[:name].nil?
-          Rails.logger.debug "No name for full task"
-          return false
-        end
-
-        # ensure show day today or in future
-        
-        if params[:show_date] < Date.current
-          Rails.logger.debug "Show date in past"
-          return false
-        end
-        
-        # create the new task
-        new_activity = nil
-        if type == Activity::FULL_TASK
-          new_activity = FullTask.create(name: params[:name], 
-                                 show_date: params[:show_date], 
-                                 description: params[:description],
-                                 expiration_date: params[:expiration_date],
-                                 reward: params[:reward],
-                                 penalty: params[:penalty])
-        elsif type == Activity::PARTIAL_TASK
-          new_activity = PartialTask.create(name: params[:name], 
-                                    show_date: [:show_date], 
-                                    description: params[:description],
-                                    expiration_date: params[:expiration_date],
-                                    reward: params[:reward],
-                                    penalty: params[:penalty])
-        end
-
-        # if it has a parent, add activity to it
-        parent_id = params[:parent_id]
-        if parent_id.empty?
-          return true
-        else
-          parent = Activity.find(parent_id)
-          parent.add_child(new_activity)
-          return true
-        end
+      when ActivityHandler::FULL_TASK 
+        return task_creator(type_id, params)
+      when ActivityHandler::PARTIAL_TASK
+        return task_creator(type_id, params)
       else
         Rails.logger.debug "Invalid type_id"
       end
+    end
+
+    def self.task_creator(type_id, params)
+      errors = { }
+      # ensure it has a show_date
+      if params[:show_date].empty?
+        Rails.logger.debug "No show date for full task"
+        errors[:show_date] = "Must include a date to show the task on"
+      elsif Date.parse(params[:show_date]) < Date.today # TIME ZONE PROBLEM
+        Rails.logger.debug "Show date in past"
+        errors[:show_date] = "Date to show task cannot be in past"
+      end
+
+      # make sure expiration date after show date
+      if !params[:expiration_date].empty? and 
+          (Date.parse(params[:expiration_date]) < Date.parse(params[:show_date]))
+        Rails.logger.debug "Expiration date before show"
+        errors[:expiration_date] = "Expiration date can't be before date of task"
+      end
+
+      # ensure it has a name
+      if params[:name].empty?
+        Rails.logger.debug "No name for full task"
+        errors[:name] = "Must have a name"
+      end
+
+      if !errors.empty?
+        return errors
+      end
+      # create the new task
+      new_activity = nil
+      if type_id == ActivityHandler::FULL_TASK
+        new_activity = FullTask.create(name: params[:name], 
+                                       show_date: params[:show_date], 
+                                       description: params[:description],
+                                       expiration_date: params[:expiration_date],
+                                       reward: params[:reward],
+                                       penalty: params[:penalty])
+      elsif type_id == ActivityHandler::PARTIAL_TASK
+        new_activity = PartialTask.create(name: params[:name], 
+                                          show_date: [:show_date], 
+                                          description: params[:description],
+                                          expiration_date: params[:expiration_date],
+                                          reward: params[:reward],
+                                          penalty: params[:penalty])
+      end
+
+      # if it has a parent, add activity to it
+      parent_id = params[:parent_id]
+      if !parent_id.empty?
+        parent = Activity.find(parent_id)
+        parent.add_child(new_activity)
+      end
+
+      return errors
+
     end
   end
 end
